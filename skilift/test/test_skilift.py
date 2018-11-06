3bd4385cc68debee59c6d53fdcbb67694384ea35
Longjiang Yang <Longjiang.Yang@target.com>
2018-11-06 13:34:17 -0600

    added slides

--- skilift/test/test_skilift.py
+++ skilift/test/test_skilift.py
@@ -17,14 +17,40 @@ def test_lift_one_bench():
     assert result == {'loaded': 4, 'num_benches': 1, 'unloaded': 0}
 
 
+@pytest.fixture
+def line_N(request):
+    line_size = request.node.get_marker('line_size').args[0]
+    return skilift.Line(line_size)
+
+
+@pytest.mark.line_size(6)
+def test_line_6(line_N):
+    result = line_N.take(7)
+    assert result == 6
+    assert line_N.num_people == 0
+
+
+@pytest.fixture
+def BenchN(request):
+    size_ = request.node.get_marker("bench_size").args[0]
+    class BSize(skilift.Bench):
+        size = size_
+    return BSize
+
+
+def test_bench6(BenchN, line_N):
+    pass
+
+
 @pytest.mark.parametrize('l, e', [
-    ([], TypeError),
+    ({'x' :1}, TypeError),
     (None, TypeError),
     ('10', TypeError),
 ])
 def test_line_bad(l, e):
     line = skilift.Line(l)
     with pytest.raises(e):
+        # line.take(1)
         line.take(1)
 
 
@@ -57,5 +83,5 @@ def test_line_sizes(l, tk, exp):
 # def test_large(large_num):
 #     assert adder(large_num, 1) == large_num
 
-@pytest.fixture(params = [-1, 0 , 100])
-def 
+# @pytest.fixture(params = [-1, 0 , 100])
+# def
